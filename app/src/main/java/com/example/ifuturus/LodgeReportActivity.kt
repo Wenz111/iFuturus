2120bd71c0dbe34a3d2c295e26c5bc124566a3c2
Wenz11 <angzw-wm17@student.tarc.edu.my>
2019-11-01 22:18:55 +0800

    Added Image Labeling and user Location

--- app/src/main/java/com/example/ifuturus/LodgeReportActivity.kt
+++ app/src/main/java/com/example/ifuturus/LodgeReportActivity.kt
@@ -1,19 +1,19 @@
 package com.example.ifuturus
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import com.google.android.gms.tasks.Continuation
 import com.google.android.gms.tasks.Task
 import com.google.android.material.chip.Chip
-import com.google.android.material.chip.ChipGroup
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
@@ -21,6 +21,18 @@ import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.activity_lodge_report.*
 import java.io.IOException
 import java.util.*
+import android.graphics.Bitmap
+import android.location.*
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import android.util.Log
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel
+import com.google.firebase.ml.vision.label.FirebaseVisionOnDeviceImageLabelerOptions
+import android.os.Build
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+private const val PERMISSION_REQUEST = 10
 
 class LodgeReportActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -30,12 +42,11 @@ class LodgeReportActivity : AppCompatActivity(), View.OnClickListener {
     private var firebaseStore: FirebaseStorage? = null
     private var storageReference: StorageReference? = null
 
-//    // Get Value From View
-//    lateinit var mchipPulic : Chip
-//    lateinit var mchipPrivate : Chip
-//    lateinit var meditTextComplaintNotes : EditText
-//    lateinit var mlocationText : EditText
-//    lateinit var mchipGroup2 : ChipGroup
+    // Permission to Access Location
+    private var permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+
+    // Get Location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,13 +58,11 @@ class LodgeReportActivity : AppCompatActivity(), View.OnClickListener {
 
         btnAddImageorVideo.setOnClickListener(this)
         btnSubmit.setOnClickListener(this)
+        chipGroup2.setOnClickListener(this)
+        btnChangeLocation.setOnClickListener(this)
 
-//        mchipPulic = findViewById(R.id.chippublic)
-//        mchipPrivate = findViewById(R.id.chipprivate)
-//
-//        meditTextComplaintNotes = findViewById(R.id.editTextComplaintNotes)
-//        mlocationText = findViewById(R.id.textViewDisplayLocation)
-//        mchipGroup2 = findViewById(R.id.chipGroup2)
+        // Get Location
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
     // Call Gallery
@@ -108,6 +117,7 @@ class LodgeReportActivity : AppCompatActivity(), View.OnClickListener {
                     addUploadRecordToDb(downloadUri.toString())
                 } else {
                     // Handle failures
+                    Log.d("Upload Tasks", "Handle Failures")
                 }
             }?.addOnFailureListener{
 
@@ -128,20 +138,112 @@ class LodgeReportActivity : AppCompatActivity(), View.OnClickListener {
             try {
                 val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, filePath)
                 imageView.setImageBitmap(bitmap)
+                analyzeImage(bitmap)
             } catch (e: IOException) {
                 e.printStackTrace()
             }
         }
     }
 
+    // Image Labeling Firebase ML Kit - Set Confidence Level 0.5F
+    private fun analyzeImage(bitmap: Bitmap) {
+        val firebaseVisionImage = FirebaseVisionImage.fromBitmap(bitmap)
+        val options = FirebaseVisionOnDeviceImageLabelerOptions.Builder()
+            .setConfidenceThreshold(0.5F)
+            .build()
+        val labelDetector = FirebaseVision.getInstance().getOnDeviceImageLabeler(options)
+        labelDetector.processImage(firebaseVisionImage)
+            .addOnSuccessListener {
+                val mutableImage = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+
+                labelImage(it, mutableImage)
+
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "There was some error", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    // Generate Image Label
+    private fun labelImage(labels: List<FirebaseVisionImageLabel>?, image: Bitmap?) {
+        if (labels == null || image == null) {
+            Toast.makeText(this, "There was some error", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        for (label in labels) {
+
+            Log.d("Image Labels", "Text: ${label.text}, Confidence: ${label.confidence}")
+
+            val chip = Chip(chipGroup2.context)
+            chip.text = "${label.text}"
+
+            // necessary to get single selection working
+            chip.isClickable = true
+            chip.isCloseIconVisible = true
+            chip.setOnCloseIconClickListener {
+                chipGroup2.removeView(it)
+            }
+
+            chipGroup2.addView(chip)
+        }
+    }
+
+    private fun resetChipGroup2() {
+        chipGroup2.removeAllViews()
+    }
+
+    // Location Permission
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST) {
+            var allSuccess = true
+            for (i in permissions.indices) {
+                if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                    allSuccess = false
+                    val requestAgain = Build.VERSION.SDK_INT >= Build.VERSION_CODES.P && shouldShowRequestPermissionRationale(permissions[i])
+                    if (requestAgain) {
+                        Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "Go to settings and enable the permission", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    // Get Location Function
+    @SuppressLint("MissingPermission")
+    private fun obtieneLocalizacion(){
+        // Change Button Text to Change Location
+        btnChangeLocation.setText("Change Location")
+
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                // Convert to Location Address
+                val geocoder : Geocoder = Geocoder(this, Locale.getDefault())
+                val addresses : List<Address>
+
+                addresses = geocoder.getFromLocation(location!!.latitude, location!!.longitude, 1) // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+
+                // Display Location Address
+                textViewDisplayLocation.setText("Latitude: ${location?.latitude}, Longitude: ${location?.longitude}")
+                textViewDisplayLocation.append("\n\nLocation Address:\n${addresses.get(0).getAddressLine(0)}")
+            }
+    }
+
 // Handle View On Click Action
     override fun onClick(v: View) {
     when (v.id) {
         R.id.btnAddImageorVideo -> {
+            resetChipGroup2()
             launchGallery()
             displayImage()
     }
-//        R.id.btnChangeLocation -> {}
+        R.id.btnChangeLocation -> {
+            requestPermissions(permissions, PERMISSION_REQUEST)
+            obtieneLocalizacion()
+        }
         R.id.btnSubmit -> {
             uploadImage()
         }
--- app/src/main/java/com/example/ifuturus/imagelabeling/imagelabelingmodel.kt
+++
@@ -1,3 +0,0 @@
-package com.example.ifuturus.imagelabeling
-
-class ImageLabelingModel(val text: String?, val confidence: Float)
